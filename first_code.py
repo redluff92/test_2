82f6dd30128b4af5f80318c38e53eb1762cc4aab
Ralf Burkhardt <mail.ralfburkhardt@web.de>
2017-10-27 18:34:30 +0200

    Add firts function

---
+++ first_code.py
@@ -0,0 +1,9 @@
+def square(x:int) -> int:
+    """
+    A quick function for testing how using git on PyCharm works
+    :param x: the number of whicj the square will be calculated
+    """
+
+    return x**2
+
+
