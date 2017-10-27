4d2d002000230ef9d754f2e8320557a0dfa7353a
Ralf Burkhardt <mail.ralfburkhardt@web.de>
2017-10-27 18:49:14 +0200

    Fix typos

--- first_code.py
+++ first_code.py
@@ -1,7 +1,8 @@
 def square(x:int) -> int:
     """
-    A quick function for testing how using git on PyCharm works
-    :param x: the number of whicj the square will be calculated
+    A quick function helping me to understand how Git and GitHub work in PyCharm
+
+    :param x: the number of which the square will be calculated
     """
 
     return x**2
